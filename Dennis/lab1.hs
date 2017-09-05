8f4a6009a1da977d6e3dc534f653132723e8d1c3
Dennis Kruidenberg <kruidenbergdennis@gmail.com>
2017-09-05 09:55:32 +0200

    _

---
+++ Dennis/lab1.hs
@@ -0,0 +1,64 @@
+-- Dennis Kruidenberg
+
+
+----- 1 -------(3 hours)
+
+import Data.List
+import Test.QuickCheck
+
+gen1 :: Gen Int
+gen1 = choose (1,10)
+
+
+rightHand2 :: Int -> Int
+rightHand2 n = (n * (n + 1) * (2 * n +1))  `div` 6
+
+tmp2 :: Int -> [Int]
+tmp2 n = map (^2) [1..n]
+
+leftHand2:: Int -> Int
+leftHand2 n = sum (tmp2 n)
+
+test2 :: Int ->  Bool
+test2 n = leftHand2 n == rightHand2 n
+        
+
+
+
+rightHand3 :: Int -> Int
+rightHand3 n = (n * (n + 1) `div` 2)^2
+
+tmp3 :: Int -> [Int]
+tmp3 n = map (^3) [1..n]
+
+leftHand3:: Int -> Int
+leftHand3 n = sum (tmp3 n)
+
+test3 :: Int ->  Bool
+test3 n = leftHand3 n == rightHand3 n
+
+
+
+--main = verboseCheck $ forAll gen1 test3
+
+
+---------3 from the workshop (10min)
+perms :: [a] ->[[a]]
+perms [] = [[]]
+perms (x:xs) = concat (map (insrt x) (perms xs)) where
+     insrt x [] = [[x]]
+     insrt x (y:ys) = (x:y:ys) : map (y:) (insrt x ys)
+
+test4 :: Int -> Bool
+test4 n = length(perms([1..n])) == product([1..n])
+--main = quickCheck $forAll gen1 test4
+
+
+-----6----
+prime :: Integer -> Bool
+prime n = n > 1 && all (\ x -> rem n x /= 0) xs
+      where xs = takeWhile (\ y -> y^2 <= n) primes
+primes :: [Integer]
+primes = 2 : filter prime [3..] 
+
+main = print(primes)
