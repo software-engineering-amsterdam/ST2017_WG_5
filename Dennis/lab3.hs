0a1fa2384ba98902534193ffbc990d8f5573ae95
Dennis Kruidenberg <kruidenbergdennis@gmail.com>
2017-09-19 14:13:02 +0200

    f

--- Dennis/lab3.hs
+++ Dennis/lab3.hs
@@ -22,9 +22,18 @@ equiv x y = tautology(Equiv x y)
 parseHelp :: String -> Form
 parseHelp s = head (parse s)
 
+--assignment 2 - 2 hours
 tester :: String -> Bool
 tester x = if length(parse x) > 0 then show (head (parse x)) == x
             else False
+
+--deMorgan :: Form -> Form
+--deMorgan f@(Prop x) = f
+--deMorgan f@(Neg (Prop x)) = f
+--deMorgan (Cnj fs) = Cnj (map deMorgan fs)
+--deMorgan (Dsj []) = Dsj []
+--deMorgan (Dsj [f]) = deMorgan f
+--deMorgan (Dsj (f:fs)) = deMorgan2 (deMorgan f) (deMorgan (Dsj fs))
 
 --main = do
 --    print ("p ^ q entails p v q")
@@ -36,8 +45,129 @@ tester x = if length(parse x) > 0 then show (head (parse x)) == x
 --    print("tautology p v -p")
 --    print(tautology (parseHelp "+(1 -1)"))
 -- precondition: No chars, only ints
-main = do
-    print(tester show(Equiv (Impl p q) (Impl (Neg q) (Neg p))))
-    print(tester show("((1==>2)<=>(-2==>-1))"))
-    print(tester "*(+(1 2) +(-2 3))")
-    print(tester "+(1 -1)")
+--main = do
+    --print(tester (show(Equiv (Impl p q) (Impl (Neg q) (Neg p)))))
+    --print(tester (show(Equiv (Cnj[Dsj[p,q],Dsj[Neg p,r]]) (Dsj[q,r]))))
+    --print(tester (show(Cnj[Dsj[p,q],Dsj[q,r],Cnj[p,r]])))
+    --print(tester "*(1 -1)")
+    --print(tester "")
+    --print(tester "23434 * 233232")
+--main = print(show(arrowfree((Impl p q)))) 
+
+--getCharacter :: Int -> ([Char], Int)
+--getCharacter a
+--    | a == 1 = ("*", a)
+--    | a == 2 = ("+", a)
+--    | a == 3 = ("==>", a)
+--    | a == 4 = ("<=>", a)
+
+--getRandom :: IO Int
+--getRandom = getStdRandom (randomR (0,4))
+
+
+--getRandomHelper:: IO ()
+--getRandomHelper = do
+--    number <- getRandom
+--    f <- getRandomOP number
+--    print(f)
+
+--getRandomOP :: Int -> IO Form
+--getRandomOP x
+--    | x == 0 = return (Neg x)
+deMorgan :: Form -> Form
+deMorgan f@(Prop x) = f
+deMorgan f@(Neg (Prop x)) = f
+deMorgan (Cnj fs) = Cnj (map deMorgan fs)
+deMorgan (Dsj []) = Dsj []
+deMorgan (Dsj [f]) = deMorgan f
+deMorgan (Dsj (f:fs)) = deMorgan2 (deMorgan f) (deMorgan (Dsj fs))
+
+deMorgan2 :: Form -> Form -> Form
+deMorgan2 (Cnj []) _ = Cnj []
+deMorgan2 (Cnj [f]) g  = deMorgan2 f g
+deMorgan2 (Cnj (f:fs)) g = Cnj [deMorgan2 f g, deMorgan2 (Cnj fs) g]
+deMorgan2  _ (Cnj []) = Cnj []
+deMorgan2 f (Cnj [g]) = deMorgan2 f g
+deMorgan2 f (Cnj (g:gs)) = Cnj [deMorgan2 f g, deMorgan2 f (Cnj gs)]
+deMorgan2 f g = Dsj [f, g]
+
+
+toCNF :: Form -> IO Form
+toCNF f = 
+    return (deMorgan (nnf (arrowfree f)))
+
+
+
+
+getCharacter :: Int -> ([Char], Int)
+getCharacter a
+    | a == 1 = ("*", a)
+    | a == 2 = ("+", a)
+    | a == 3 = ("==>", a)
+    | a == 4 = ("<=>", a)
+
+
+genPropInt :: IO Int
+genPropInt = getStdRandom (randomR (-10,10))
+
+genOp :: IO Int
+genOp = getStdRandom (randomR (1,4))
+
+genCounter :: IO Int
+genCounter = getStdRandom (randomR (0,3))
+
+genProposition :: IO Form
+genProposition = do
+    a <- genCounter
+    f <- (genPropHelper a)
+    return f
+
+
+genPropHelper :: Int -> IO Form
+-- base case
+genPropHelper 0 = do
+    a <- genPropInt
+    return (Prop a)
+
+genPropHelper counter = do
+    a <- genOp
+    case a of 
+        1 -> do
+            prop1 <- (genPropHelper (counter - 1))
+            prop2 <- (genPropHelper (counter - 1))
+            return (Cnj [prop1, prop2])
+        2 -> do
+            prop1 <- (genPropHelper (counter - 1))
+            prop2 <- (genPropHelper (counter - 1))
+            return (Dsj [prop1, prop2])
+        3 -> do
+            prop1 <- (genPropHelper (counter - 1))
+            prop2 <- (genPropHelper (counter - 1))
+            return (Impl prop1 prop2)
+        4 -> do
+            prop1 <- (genPropHelper (counter - 1))
+            prop2 <- (genPropHelper (counter - 1))
+            return (Equiv prop1 prop2)
+
+cnfTester ::  IO()
+cnfTester = do 
+    a <- genProposition
+    b <- toCNF a
+    let result = tautology(Equiv a b)
+    if result then print("pass on: " ++ show(a) ++ " == " ++ show(b)) else print("fail on: " ++ show(a))
+     
+
+
+
+    
+
+--randomProp :: [Char]
+--randomProp = "(" ++ (show 1) ++ getOp ++ (show 3) ++ ")"
+--randomProp = getOp ++ "(" ++ (show 1) ++ (show 3) ++ ")"
+
+main = cnfTester 
+
+
+
+
+
