e8e43e4ba22836d28027a48cd35d50b580570d0d
tdobber <timo.dobber@student.uva.nl>
2017-10-13 11:07:36 +0200

    Added 5-7

--- Timo/Lab6.hs
+++ Timo/Lab6.hs
@@ -1,46 +1,9 @@
+module Lab6 where
 
+import Data.List
+import System.Random
 import Lecture6
--- assignment 2 test report:
--- We execute the same tests using both implementations 100.000 times with the arguments:
--- x = 20000
--- y = 43000000
--- n = 301
--- 
--- These variables are chosen simply because they are very large, and it is well known that the algorithm used is
--- more efficient for large exponentional numbers. 
---
--- executing the test (main2) 100.000 times for expM:
--- (8.06 secs, 1.380.801.752 bytes)
---
--- executing the test (main3) 100.000 times for exM:
--- (0.10 secs, 44835960 bytes)
---
--- The first main is used to let haskell load all the packages and not affect the execution time of the other functions
---
--- We can clearly see that our own implementation is a lot faster, namely 80.6 times for such large numbers. 
 
-testExM :: Integer -> Integer -> Integer -> Integer -> Integer
-testExM x y n 0 = exM x y n
-testExM x y n num_tests = do
-	let f = exM x y n
-	if num_tests /= 0 
-		then (testExM x (y+1) n (num_tests-1))
-		else (testExM x (y+1) n (num_tests-1))
-
-testExpM :: Integer -> Integer -> Integer -> Integer -> Integer
-testExpM x y n 0 = expM x y n
-testExpM x y n num_tests = do
-	let f = expM x y n
-	if num_tests /= 0 
-		then (testExpM x (y+1) n (num_tests-1))
-		else (testExpM x (y+1) n (num_tests-1))
-
-
-main1= print "haskell"
-main2 = print (testExpM 20000 43000000 301 100000)
-main3 = print (testExM 20000 43000000 301 100000)
-
-----------------------------------------------------------------------------------
 -- Assignment 5 (Time spent: 2 hours)
 -- Run: assignment5 carmichael
 -- Carmichael numbers are composite nnumbers that are made in such a way,
@@ -65,7 +28,6 @@ assignment5 (x:xs) = do
         else return ()
     assignment5 xs
 
-----------------------------------------------------------------------------------
 -- Assignment 6 (Time spent: 30 minutes)
 -- Run: assignment6 carmichael
 -- The Miller-Rabin primality check performs better than the Fermats test.
@@ -82,7 +44,6 @@ assignment6 (x:xs) = do
         else return ()
     assignment6 xs
 
-----------------------------------------------------------------------------------
 -- Assignment 7 (Time spent: 1 hour)
 -- Run: assignment7 primes
 -- We use the prime number generator "primes" from the lecture code.
